ae167e362be29d83a3c9b4be95896fc0a987fbec
Yuki N <lunarbase@outlook.com>
2018-10-30 18:58:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(num)
+  if num <= 1
+   return false
+  end
+  val = Math.sqrt(num).to_i
+  list = (2..val).to_a 
+  
+    list.each {|i| return false if num % i == 0}
+  true
+end
